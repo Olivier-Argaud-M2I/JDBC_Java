03b9e45dbca737ac75419d37cd45c16f5e266402
Olivier-Argaud-M2I <Olivier.ARGAUD@labom2iformation.fr>
2022-08-03 14:53:08 +0200

    ajout dataSource

--- src/main/java/fr/m2i/jdbc/servlets/TodoServlet.java
+++ src/main/java/fr/m2i/jdbc/servlets/TodoServlet.java
@@ -2,26 +2,83 @@ package fr.m2i.jdbc.servlets;
 
 import fr.m2i.jdbc.models.ActorDto;
 import fr.m2i.jdbc.models.Todo;
+import fr.m2i.jdbc.utils.MaConnexionTodo;
 import fr.m2i.jdbc.utils.MesRequetes;
 import fr.m2i.jdbc.utils.MesRequetesTodo;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(name = "TodoServlet", value = "/todos")
 public class TodoServlet extends HttpServlet {
 
+    @Resource(name = "dataSource")
+    private DataSource dataSource;
+
     private static final String PAGE = "/WEB-INF/pages/pageTodo.jsp";
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        request.setAttribute("todos",MesRequetesTodo.getTodos());
+
+
+
+
+        List<Todo> todos = new ArrayList<>();
+        try{
+            Connection connection = dataSource.getConnection();
+            Statement statement = connection.createStatement();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM todo");
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next()){
+                todos.add(new Todo(
+                        resultSet.getInt("id"),
+                        resultSet.getString("nom"),
+                        resultSet.getString("description")
+                ));
+            }
+
+            statement.close();
+            connection.close();
+
+        }catch(Exception e ){
+            System.out.println("oups");
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        request.setAttribute("todos",todos);
+
+
+
+
+//        request.setAttribute("todos",MesRequetesTodo.getTodos());
 
         this.getServletContext().getRequestDispatcher(PAGE).forward(request, response);
     }
--- src/main/java/fr/m2i/jdbc/utils/MaConnexionTodo.java
+++ src/main/java/fr/m2i/jdbc/utils/MaConnexionTodo.java
@@ -1,6 +1,10 @@
 package fr.m2i.jdbc.utils;
 
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -13,11 +17,23 @@ public class MaConnexionTodo {
 
     private static Connection connection;
 
+    private static DataSource dataSource;
+
+    static{
+        try {
+            Context envContext = InitialContext.doLookup("java:/comp/env");
+            dataSource = (DataSource) envContext.lookup("dataSource");
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static Connection getConnexion() throws SQLException {
 
         if(connection==null){
-            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-            connection = DriverManager.getConnection(BDD,LOGIN,MDP);
+//            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+//            connection = DriverManager.getConnection(BDD,LOGIN,MDP);
+            connection = dataSource.getConnection();
         }
         return connection;
     }
